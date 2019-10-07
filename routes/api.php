c89aafdd7f3edd5ddd176bfa19b576d2f7adea39
wywangwei <wang1230yang1230@163.com>
2019-10-07 10:35:28 +0800

    first plugin commit

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,16 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+$api = app('Dingo\Api\Routing\Router');
+$api->version('v1', function ($api) {
+    $api->post('login', 'App\Http\Api\Auth\LoginController@login');
+    $api->post('register', 'App\Http\Api\Auth\RegisterController@register');
+    $api->group(['namespace'=>'App\Http\Controller\Api'],function($api){
+        $api->get('/',function (){
+            echo "myApi";
+        });
+        $api->get('logout','App\Http\Api\Auth\LoginController@logout');
+        $api->resource('user','App\Http\Api\UsersController');
+    });
+    $api->get('refresh','App\Http\Api\UsersController@refresh');
+});
